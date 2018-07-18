a0db6256ecd06f130b00f976ecbeee14706d5c84
Celine Sun <Celine@Celines-MacBook-Air.local>
2018-07-18 13:57:44 +0800

    message

---
+++ clock/clock/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  clock
+//
+//  Created by Celine Sun on 2018/7/16.
+//  Copyright © 2018年 Celine Sun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+
+    
+
+
+
+}
